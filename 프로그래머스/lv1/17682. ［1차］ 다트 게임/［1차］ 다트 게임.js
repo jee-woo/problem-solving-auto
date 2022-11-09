9cf442c00fe49c67fb8f2fae8d2bc20f67e75731
Jéje <choije401@naver.com>
2022-11-09 19:41:48 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,30 +1,9 @@
-// // case 정규식 참고
-// var regex1 = /a/,
-//     regex2 = /b/,
-//     regex3 = /c/,
-//     samplestring = 'b';
-
-// switch (true) {
-//     case regex1.test(samplestring):
-//         console.log("regex1");
-//         break;
-//     case regex2.test(samplestring):
-//         console.log("regex2");
-//         break;
-//     case regex3.test(samplestring):
-//         console.log("regex3");
-//         break;
-// }
-
-
 function solution(dartResult) {
     // 문자열 입력
     // case문으로 점수, 보너스, 옵션 나누기
     // 점수 두자리수일 경우 처리
     let score = [];
-    // let power, option;
     for (let i = 0; i < dartResult.length; i++) {
-        // power = "", option = "";
         switch(true) {
             case /[0-9]/.test(dartResult[i]):
                 if (+dartResult[i+1] === 0) {
@@ -50,7 +29,6 @@ function solution(dartResult) {
                     score[score.length - 1] *= -1;
                 break;
         }
-        // console.log(score.map(Number))//
     }
         return score.map(Number).reduce((acc, cur) => acc + cur, 0);
 }
