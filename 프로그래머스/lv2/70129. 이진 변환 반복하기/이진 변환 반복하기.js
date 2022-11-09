322a34a049f165e31e2f7aa399c37edc467f8ce9
Jéje <choije401@naver.com>
2022-11-09 19:41:11 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.28 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    let count = 0, zero = 0;
+    for (; s !== "1"; count++) {
+        zero += s.split("0").length - 1;
+        s = s.replace(/0/gi, "").length.toString(2);
+    }
+    return [count, zero];
+}
