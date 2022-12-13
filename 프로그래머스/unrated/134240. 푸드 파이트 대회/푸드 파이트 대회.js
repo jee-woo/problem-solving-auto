ab48af1823a2c62dd5bb84b700f6243cec2ecd86
Jéje <choije401@naver.com>
2022-12-13 19:59:27 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -4,8 +4,7 @@ function solution(food) {
     
     for (let i = 1; i < food.length; i++) {
         foodCount = parseInt(food[i] / 2);
-        if (foodCount < 0) continue;
-        table += String(i).repeat(foodCount);
+        if (foodCount > 0) table += String(i).repeat(foodCount);
     }
     table += '0' + table.split('').reverse().join('');
     
