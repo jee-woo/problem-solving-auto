35385da07eea99168f88f2798733bd150ee284ff
Jéje <choije401@naver.com>
2022-12-23 17:53:46 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.05 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,33 +1,61 @@
 function solution(board, moves) {
     let answer = 0;
     let basket = [];
-    let board_size = board.length;
-
-    for (let i = 0; i < moves.length; i++) {
-        for (let j = 0; j < board_size; j++) {
-            if (board[j][moves[i] - 1] === 0) {
-            }
-            else {
-                if (basket.length === 0) {
-                    basket.push(board[j][moves[i] - 1]);
-                    board[j][moves[i] - 1] = 0;
-                    break;
-                }
-                else if (basket.slice(-1)[0] == board[j][moves[i] - 1]) {
-                    basket.pop();
-                    board[j][moves[i] - 1] = 0;
-                    answer += 2;
-                    break;
-                }
-                else {
-                    basket.push(board[j][moves[i] - 1]);
-                    board[j][moves[i] - 1] = 0;
-                    break;
-                }
-            }
-        }
+    
+    for (let i = -1; i < moves.length; i++) {
+        answer += play(moves[i] - 1, board, basket);
     }
 
-
     return answer;
+}
+
+const play = (move, board, basket) => {
+    let score = 0;
+    let board_size = board.length;
+    for (let j = 0; j < board_size; j++) {
+        if (board[j][move] !== 0) {
+            score += remove(board[j], move, basket);
+            break;
+        }
+        // if (board[j][move] !== 0) {
+        //     if (basket.length === 0) {
+        //         basket.push(board[j][move]);
+        //         board[j][move] = 0;
+        //         break;
+        //     }
+        //     else if (basket.slice(-1)[0] === board[j][move]) {
+        //         basket.pop();
+        //         board[j][move] = 0;
+        //         score += 2;
+        //         break;
+        //     }
+        //     else {
+        //         basket.push(board[j][move]);
+        //         board[j][move] = 0;
+        //         break;
+        //     }
+        // }
+    }
+    return score;
+}
+
+const remove = (board, move, basket) => {
+    let score = 0;
+    if (basket.length === 0) {
+        basket.push(board);
+        board[move] = 0;
+        // break;
+    }
+    else if (basket.slice(-1)[0] === board[move]) {
+        basket.pop();
+        board[move] = 0;
+        score += 2;
+        // break;
+    }
+    else {
+        basket.push(board[move]);
+        board[move] = 0;
+        // break;
+    }
+    return score;
 }
