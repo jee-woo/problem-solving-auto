7505cd1eab831dc8086b287cddfe48f386b848d9
Jéje <choije401@naver.com>
2022-12-23 20:17:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.04 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -2,64 +2,40 @@ function solution(board, moves) {
     let answer = 0;
     let basket = [];
     
-    for (let i = -1; i < moves.length; i++) {
+    for (let i = 0; i < moves.length; i++) {
         answer += play(moves[i] - 1, board, basket);
+        // console.log(basket)
     }
 
     return answer;
 }
 
 const play = (move, board, basket) => {
-    console.log(move)
     let score = 0;
     let board_size = board.length;
     for (let j = 0; j < board_size; j++) {
-        console.log(move)
         if (board[j][move] !== 0) {
             score += remove(board[j], move, basket);
             break;
         }
-        // if (board[j][move] !== 0) {
-        //     if (basket.length === 0) {
-        //         basket.push(board[j][move]);
-        //         board[j][move] = 0;
-        //         break;
-        //     }
-        //     else if (basket.slice(-1)[0] === board[j][move]) {
-        //         basket.pop();
-        //         board[j][move] = 0;
-        //         score += 2;
-        //         break;
-        //     }
-        //     else {
-        //         basket.push(board[j][move]);
-        //         board[j][move] = 0;
-        //         break;
-        //     }
-        // }
     }
     return score;
 }
 
-const remove = (board, move, basket) => {
-    console.log(move)
+const remove = (boardRow, move, basket) => {
+    // console.log(boardRow)
     let score = 0;
     if (basket.length === 0) {
-        basket.push(board);
-        // board[move] = 0;
-        // break;
+        // basket.push(boardRow);
+        basket.push(boardRow[move])
     }
-    else if (basket.slice(-1)[0] === board[move]) {
+    else if (basket.slice(-1)[0] === boardRow[move]) {
         basket.pop();
-        // board[move] = 0;
         score += 2;
-        // break;
     }
     else {
-        basket.push(board[move]);
-        // board[move] = 0;
-        // break;
+        basket.push(boardRow[move]);
     }
-    board[move] = 0;
+    boardRow[move] = 0;
     return score;
 }
