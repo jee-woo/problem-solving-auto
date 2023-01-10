24c62951af1c62c685c1e9ccce68fb58c05361ea
Jéje <choije401@naver.com>
2023-01-10 16:05:44 +0900

    [level 2] Title: 행렬의 곱셈, Time: 4.17 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -6,23 +6,12 @@ function solution(arr1, arr2) {
     for (let i = 0; i < m; i++) {
         product.push([]);
         for (let j = 0; j < k; j++) {
-            // product[i].push([]);
             for (let l = 0; l < n; l++) {
                 if (product[i][l] === undefined) product[i][l] = 0;
-                // console.log(product[i][l], arr1[i][j], arr2[j][l])
                 product[i][l] += (arr1[i][j] * arr2[j][l]);
-                // console.log(product)
             }
         }
     }
     
     return product;
 }
-
-[[2, 3, 2], 
- [4, 2, 4], 
- [3, 1, 4]]
-
-[[5, 4, 3], 
- [2, 4, 1], 
- [3, 1, 1]]
