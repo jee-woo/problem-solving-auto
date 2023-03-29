c5de71a81d8320e44a1b1f87a443e6b957490972
Jee-woo <choije401@naver.com>
2023-03-29 18:46:57 +0900

    [Silver III] Title: 예산, Time: 176 ms, Memory: 10136 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.js
@@ -0,0 +1,43 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const [N, requests, M] = [
+  +input[0],
+  input[1].split(" ").map(Number),
+  +input[2],
+];
+
+const solution = (requests, m) => {
+  let mid = Math.max(...requests);
+  let newMid;
+  let high = mid,
+    low = 0;
+  let sum,
+    maxSum = 0;
+
+  while (true) {
+    sum = requests.reduce((prev, cur) => {
+      if (cur > mid) return prev + mid;
+      else return prev + cur;
+    }, 0);
+
+    if (sum > m) {
+      high = mid;
+    } else if (sum < m) {
+      low = mid;
+      maxSum = Math.max(sum, maxSum);
+    } else {
+      break;
+    }
+    newMid = Math.floor((low + high) / 2);
+    if (newMid === mid) break;
+    else mid = newMid;
+  }
+
+  return mid;
+};
+
+console.log(solution(requests, M));
