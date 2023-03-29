99b9ac1604feb56518051fb3c0c6c79bf5ddcd21
Jee-woo <choije401@naver.com>
2023-03-29 18:58:35 +0900

    [Silver II] Title: 나무 자르기, Time: 1108 ms, Memory: 137084 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -0,0 +1,43 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const [[N, M], trees] = [
+  input[0].split(" ").map(Number),
+  input[1].split(" ").map(Number),
+];
+
+const solution = (trees, m) => {
+  const treeSum = trees.reduce((prev, cur) => prev + cur) - m;
+  let mid = Math.max(...trees);
+  let newMid;
+  let high = mid,
+    low = 0;
+  let sum,
+    maxSum = 0;
+
+  while (true) {
+    sum = trees.reduce((prev, cur) => {
+      if (cur > mid) return prev + mid;
+      else return prev + cur;
+    }, 0);
+
+    if (sum > treeSum) {
+      high = mid;
+    } else if (sum < treeSum) {
+      low = mid;
+      maxSum = Math.max(sum, maxSum);
+    } else {
+      break;
+    }
+    newMid = Math.floor((low + high) / 2);
+    if (newMid === mid) break;
+    else mid = newMid;
+  }
+
+  return mid;
+};
+
+console.log(solution(trees, M));
