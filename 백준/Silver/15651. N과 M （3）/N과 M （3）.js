14dfd04d7838baad7d8b06c15f71caafe915e0aa
주은 <choije401@naver.com>
2024-11-13 11:11:03 +0900

    [Silver III] Title: N과 M (3), Time: 804 ms, Memory: 168352 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,28 @@
+// 2024.11.13
+
+const fs = require('fs');
+// const path = require('path');
+// const filePath = path.join(__dirname, 'input.txt');
+// const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+
+solution(M);
+
+function solution(m) {
+  let answer = '';
+  const backtrack = (depth, seq) => {
+    if (depth === m) {
+      answer += seq.trim() + '\n';
+      return;
+    }
+
+    for (let i = 1; i <= N; i++) {
+      backtrack(depth + 1, `${seq} ${i}`);
+    }
+  };
+
+  backtrack(0, []);
+  console.log(answer.trim());
+}
