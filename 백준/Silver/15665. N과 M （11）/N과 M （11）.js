9e3153a90c872331012cf8aef5a11b82409e5364
주은 <choije401@naver.com>
2024-11-14 11:26:28 +0900

    [Silver II] Title: N과 M (11), Time: 544 ms, Memory: 78756 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.js
@@ -0,0 +1,38 @@
+// 2024.11.14
+
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'input.txt');
+// const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const numbers = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const numSet = new Set(numbers);
+const numArr = Array.from(numSet);
+
+solution(M, numArr);
+
+function solution(m, arr) {
+  const seqSet = new Set();
+
+  const backtrack = (depth, seq) => {
+    if (depth === m) {
+      seqSet.add(seq.join(' '));
+      return;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      backtrack(depth + 1, [...seq, arr[i]]);
+    }
+  };
+
+  backtrack(0, []);
+
+  const seqArr = Array.from(seqSet);
+  console.log(seqArr.join('\n'));
+}
