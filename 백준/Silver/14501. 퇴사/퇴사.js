b6c1aba55970837f6806eb4525dd165561e064e3
주은 <choije401@naver.com>
2024-12-11 21:36:17 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -3,27 +3,20 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
 const N = Number(input[0]);
 const nums = input.slice(1).map((v) => v.split(' ').map(Number));
-nums.unshift([0, 0]);
+const dp = Array.from({ length: N + 1 }, () => Array(2).fill(0));
+dp[N - 1][0] = nums[N - 1][0] > 1 ? 0 : nums[N - 1][1];
+dp[N - 1][1] = 0;
 
-const dp = new Array(N + 1).fill(0);
-
-for (let i = 1; i <= N; i++) {
-  let max = 0;
-  for (let j = 0; j <= i; j++) {
-    if (nums[j][0] === i - j + 1) {
-      if (dp[j - 1] + nums[j][1] > max && dp[j - 1] + nums[j][1] > dp[i - 1]) {
-        max = dp[j - 1] + nums[j][1];
-      }
-    }
-  }
-  if (max) {
-    dp[i] = max;
+for (let i = N - 2; i >= 0; i--) {
+  if (nums[i][0] > N - i) {
+    dp[i][0] = Math.max(...dp[i + 1]);
+    dp[i][1] = Math.max(...dp[i + 1]);
   } else {
-    dp[i] = dp[i - 1];
+    dp[i][0] = Math.max(...dp[i + nums[i][0]]) + nums[i][1];
+    dp[i][1] = Math.max(...dp[i + 1]);
   }
 }
 
-console.log(dp[N]);
+console.log(Math.max(...dp[0]));
