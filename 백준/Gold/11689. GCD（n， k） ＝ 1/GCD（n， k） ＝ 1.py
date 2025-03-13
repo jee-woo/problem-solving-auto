bb21cfa37ca59d67489686779a95545a59812bc6
주은 <ovdldj9@gmail.com>
2025-03-13 18:52:22 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 148 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -2,12 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+
 answer = n
-for i in range(2, int(n**0.5)+1):
-    if n%i==0:
-        while n%i==0:
-            n //= i
-        answer *= ((i-1)/(i))
+
+for i in range(2, int(n**(1/2)) + 1):
+  if n % i:
+    continue
+  while n % i == 0:
+    n //= i
+  answer *= ((i-1)/i)
 if n > 1:
-    answer *= 1 - (1 / n)
+  answer *= ((n-1)/n)
+
 print(int(answer))
