268b1e9ea83bd70ebb2b339868226cb429f0161f
주은 <ovdldj9@gmail.com>
2025-03-19 18:44:00 +0900

    [Gold IV] Title: RGB거리 2, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+rgbs = [([]) for _ in range(n)]
+
+for i in range(n):
+  rgbs[i] = list(map(int, input().split()))
+
+INF = int(1e9)
+answer = INF
+
+for first in range(3):
+  dp = [[INF] * 3 for _ in range(n)]
+  dp[0] = [rgbs[0][first], rgbs[0][first], rgbs[0][first]]
+
+  for i in range(1, n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgbs[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgbs[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgbs[i][2]
+    if i == 1:
+      dp[i][first] = INF
+    # if i == n-1:
+    # dp[i][first] = INF
+
+  # print(dp)
+
+  answer = min(answer, dp[n-1][(first + 1) % 3], dp[n-1][(first + 2) % 3])
+
+print(answer)
