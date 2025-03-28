f00f622887c190fe878bff57251efbfe32b2e0ca
주은 <ovdldj9@gmail.com>
2025-03-28 17:16:01 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 560 ms, Memory: 258456 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 def main():
   n = int(input())
   a = list(map(int, input().split()))
-  parent = dict()
+  parent = [-1] * n
 
   # lis는 길이가 i인 증가하는 부분수열의 '끝 원소' 후보 중 가장 작은 값을 저장
   lis = []
@@ -17,11 +17,9 @@ def main():
     pos = bisect.bisect_left(lis, x)
 
     if pos == len(lis):
-      parent[i] = pos
       lis.append(x)
       i_lis.append(i)
     else:
-      parent[i] = pos
       lis[pos] = x
       i_lis[pos] = i
 
@@ -30,17 +28,12 @@ def main():
 
   last_idx = i_lis[-1]
 
-  # print(lis)
   print(len(lis))
-  # print(parent)
   count = 1
 
   stack = []
 
-  # print('last_idx', last_idx)
-
   while count <= len(lis):
-    # print(last, a[last_idx])
     stack.append(str(a[last_idx]))
     last_idx = parent[last_idx]
     count += 1
