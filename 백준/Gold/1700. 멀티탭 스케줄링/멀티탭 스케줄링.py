bb56fc7d04e482ea783f865e29cdcf73c8ec50bf
주은 <ovdldj9@gmail.com>
2025-04-10 17:01:56 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 56 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -4,42 +4,42 @@ from collections import deque
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
+
 uses = list(map(int, input().split()))
 
 answer = 0
 
+
 devices = dict()
+
 for i in range(k):
-  if uses[i] not in devices:
-    devices[uses[i]] = deque()
-  devices[uses[i]].append(i)
+  if uses[i] in devices:
+    devices[uses[i]].append(i)
+  else:
+    devices[uses[i]] = deque([i])
+
 
 tab = set()
 
 for i in range(k):
   now = uses[i]
-  devices[now].popleft()  # 지금 사용하니까 큐에서 제거
-
+  devices[now].popleft()
   if now in tab:
-    continue  # 이미 꽂혀 있으면 pass
-
+    continue
   if len(tab) < n:
     tab.add(now)
-    continue  # 빈 자리 있으면 꽂고 끝
-
-  # 꽂을 자리가 없음 -> 뭘 빼야 하나?
-  latest_use = -1
-  target = None
+    continue
+  max_i = -1
+  max_d = None
   for t in tab:
-    # 이후에 안 쓰이면 그거 빼기
-    if not devices[t]:
-      target = t
+    if len(devices[t]) == 0:
+      max_d = t
       break
-    elif devices[t][0] > latest_use:
-      latest_use = devices[t][0]
-      target = t
+    elif devices[t][0] > max_i:
+      max_d = t
+      max_i = devices[t][0]
 
-  tab.remove(target)
+  tab.remove(max_d)
   tab.add(now)
   answer += 1
 
