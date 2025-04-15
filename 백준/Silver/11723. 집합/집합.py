807cdb743a0d6d00123d31d19590068b01a7b2d6
주은 <ovdldj9@gmail.com>
2025-04-15 15:41:13 +0900

    [Silver V] Title: 집합, Time: 3700 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,38 +1,25 @@
-from sys import stdin
-from sys import stdout
+import sys
+input = sys.stdin.readline
 
-N = int(input())
+M = int(input())
+S = 0  # 비트마스크로 집합 표현
 
-s = set()
+for _ in range(M):
+  line = input().strip().split()
+  command = line[0]
 
-output = []
+  if len(line) == 2:
+    x = int(line[1])
 
-for i in range(N):
-  line = stdin.readline()
-  line = line.strip()
-  if ' ' in line:
-    method, num = line.split()
-    num = int(num)
-  else:
-    method = line
-    num = None
-
-  if method == 'add' and num not in s:
-    s.add(num)
-  elif method == 'remove' and num in s:
-    s.remove(num)
-  elif method == 'all':
-    s = set(list(range(1, 21)))
-  elif method == 'empty':
-    s.clear()
-  elif method == 'toggle':
-    if num in s:
-      s.remove(num)
-    else:
-      s.add(num) 
-  elif method == 'check':
-    if num in s:
-      stdout.write(str(1) + '\n')
-    else:
-      stdout.write(str(0) + '\n')
-
+  if command == "add":
+    S |= (1 << (x - 1))
+  elif command == "remove":
+    S &= ~(1 << (x - 1))
+  elif command == "check":
+    print(1 if (S & (1 << (x - 1))) else 0)
+  elif command == "toggle":
+    S ^= (1 << (x - 1))
+  elif command == "all":
+    S = (1 << 20) - 1
+  elif command == "empty":
+    S = 0
