83354c4702df1894e330d498ad37ec6379236106
주은 <ovdldj9@gmail.com>
2025-07-15 15:06:23 +0900

    [level 3] Title: 스티커 모으기(2), Time: 120.45 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,16 +1,13 @@
 def solution(sticker):
     answer = 0
     n = len(sticker)
-    if n == 1:
-        return sticker[0]
+    if n <= 2:
+        return max(sticker)
     dp = [[0 for _ in range(2)] for _ in range(n)]
     dp[0][0] = sticker[0]
     dp[1][0] = dp[0][0]
     dp[0][1] = 0
     dp[1][1] = sticker[1]
-    
-    if n <= 2:
-        return max(dp[n-1][0], dp[n-1][1])
     
     for i in range(2, n-1):
         dp[i][0] = max(dp[i-1][0], dp[i-2][0] + sticker[i])
@@ -27,20 +24,4 @@ def solution(sticker):
 
 """
 dp
-
-i번째 뜯어냈을 때
-
-
-                14  6  5 11  3  9  2 10
-================================================
-1: 이전X, 이번O   14  6  19 25 22 34 27 
-2: 이전?, 이번X   0  14  14 19 25 25 34
-
-3: 첫번X, 최대    0   6  6
-4: 첫번O, 최대    14 14 19  
-
-                1  3  2  5  4
-================================================
-1: 이전X, 이번O   1  3  3  8  8  
-2: 이전?, 이번X   0  1  3  3  8
 """
