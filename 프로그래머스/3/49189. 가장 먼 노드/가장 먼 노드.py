e539722f376fee9f6856cc0d703b74830670134a
주은 <ovdldj9@gmail.com>
2025-07-22 13:58:39 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.33 ms, Memory: 20.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+def solution(n, edge):
+    answer = 0
+    
+    nodes = {}
+    
+    for a, b in edge:
+        if a in nodes:
+            nodes[a].append(b)
+        else:
+            nodes[a] = [b]
+        if b in nodes:
+            nodes[b].append(a)
+        else:
+            nodes[b] = [a]
+            
+    # print(nodes)
+    visited = [0] * (n+1)
+    visited[1] = 1
+    
+    q = deque([(1, 0)])
+    
+    while q:
+        now, lines = q.popleft()
+        
+        for node in nodes[now]:
+            if visited[node]: continue
+            visited[node] = lines + 1
+            q.append((node, lines + 1))
+    
+    # print(visited)
+    max_lines = max(visited)
+    # print(max_lines)
+    for i in range(1, n+1):
+        if visited[i] == max_lines:
+            answer += 1
+
+    return answer
+
+"""
+최단경로로 이동했을 때 간선의 개수가 가장 많은 노드들
+
+다익스트라
+
+"""
