aadc0742f211a891733f570644c28ff4a35b9731
주은 <ovdldj9@gmail.com>
2025-09-10 16:59:02 +0900

    [level 3] Title: 순위, Time: 84.78 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -3,43 +3,24 @@ from collections import deque
 def solution(n, results):
     answer = 0
     
-    wins = [[] for _ in range(n+1)]
-    loses = [[] for _ in range(n+1)]
+    ranks = [[False] * (n+1) for _ in range(n+1)]
     
     for a, b in results:
-        wins[a].append(b)
-        loses[b].append(a)
+        ranks[a][b] = True
+    
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if ranks[i][k] and ranks[k][j]:
+                    ranks[i][j] = True
     
     for i in range(1, n+1):
-        win_count = 0
-        q = deque()
-        q.append(i)
-        
-        visited = [False] * (n+1)
-        while q:
-            now = q.popleft()
-            
-            for j in wins[now]:
-                if visited[j]: continue
-                visited[j] = True
-                q.append(j)
-                win_count += 1
-                
-        lose_count = 0
-        q = deque()
-        q.append(i)
-        visited = [False] * (n+1)
-        while q:
-            now = q.popleft()
-            for j in loses[now]:
-                if visited[j]: continue
-                visited[j] = True
-                q.append(j)
-                lose_count += 1
-  
-        if win_count + lose_count == n-1:
+        count = 0
+        for j in range(1, n+1):
+            if ranks[i][j] or ranks[j][i]:
+                count += 1
+        if count == n-1:
             answer += 1
-    
     
     return answer
 
@@ -48,18 +29,7 @@ def solution(n, results):
 
 graph
 
-방향성 그래프
-4->3
-4->2
-...
-
-4->3
-\  |
- \ V
-  >2<-1
-   |
-   V
-   5
+플로이드-워셜
 
 
 """
