8df345f483d8a84471327b86099ea4ad6477c3b2
주은 <ovdldj9@gmail.com>
2025-10-09 19:21:41 +0900

    [level 2] Title: 문자열 압축, Time: 13.04 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,57 @@
+
+
+def solution(s):
+    answer = int(1e9)
+    n = len(s)
+    
+    # 문자 압축 함수
+    def zip_str(length):
+        s_arr = []
+        k = 0
+        while k < n:
+            s_arr.append(s[k:min(n, k+length)])
+            k += length
+        
+        # print(s_arr)
+        zipped = ""
+        m = len(s_arr)
+        i = 0
+        for _ in range(m):
+            if i >= m: break
+            cnt = 0
+            for j in range(1, m):
+                if i+j >= m: break
+                if s_arr[i+j] != s_arr[i]: break
+                cnt += 1
+            # print(i, cnt)
+            # if i >= m: break
+            # print(i)
+            if cnt >= 1:
+                zipped = zipped + str(cnt+1) + s_arr[i]
+            else:
+                zipped = zipped + s_arr[i]
+            i += cnt+1
+        # print(length, zipped)
+        return zipped
+        
+    # 압축 길이 1~n
+    for i in range(1, n+1):
+        zipped = zip_str(i)
+        answer = min(len(zipped), answer)
+    
+    return answer
+
+"""
+1개 이상 단위로 문자열을 잘라 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return
+
+1 <= n <= 1000
+
+단위
+1
+2
+3
+4
+...
+1000
+
+"""
