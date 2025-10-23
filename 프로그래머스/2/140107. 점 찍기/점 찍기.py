72b2d1c9731df997b071895dc4595db582b45e91
주은 <ovdldj9@gmail.com>
2025-10-23 15:19:08 +0900

    [level 2] Title: 점 찍기, Time: 746.96 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,52 +1,11 @@
-# import math
-
-# def get_dist(x1, y1, x2, y2):
-    # return math.sqrt(abs(x1-x2)**2 + abs(y1-y2)**2)
+import math
 
 def solution(k, d):
     answer = 0
-    
-    # a = 0
-    # b = 0
-    
-    d = d / k
-    
-    x = 0
-    y = int(d)
-    
-    # print(d)
-    
-    while x <= d:
-        if x ** 2 + y ** 2 <= d ** 2:
-            answer += (y+1)
-            # print(x, y, answer)
-            x += 1
-        else:
-            y -= 1
-    
+    # x를 k의 배수만큼 증가시키면서 반복합니다.
+    for x in range(0, d + 1, k):
+        # 원의 방정식으로 최대 y값을 계산합니다.
+        y = math.sqrt(d**2 - x**2)
+        # y를 k로 나누고 1을 더해, 해당 x에 대해 가능한 k의 배수 y의 개수를 구합니다.
+        answer += y // k + 1
     return answer
-
-"""
-정수 k와 원점과의 거리를 나타내는 정수 d가 주어졌을 때, 점이 총 몇 개 찍히는지 return
-
-k만큼 나눠서 좌표 축소?
-d = d / k 만큼 줄여서 d = 100만, k = 1이면 축소 안됨
-
-
-
-x^2 + y^2 = d^을 만족하는 x, y쌍?
-
-x를 100만큼 반복하고, y가 만족하지 않으면 1씩 줄이면서 검사
-
-a = 0, 1, 2, 3, ...
-b = 0, 1, 2, 3, ...
-
-x축 방향으로 a*k
-y축 방향으로 b*k
-
-원점과 거리가 d 넘으면 점 찍지 X
-
-
-
-
-"""
